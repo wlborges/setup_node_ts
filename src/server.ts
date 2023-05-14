5f64bc264f2c05252df89aff2667da4d94c1e3ff
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2023-05-14 20:28:37 -0300

    porta

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
 import '@controllers/UserController'
+import express from 'express'
 
 const app = express()
 app.get('/', (req, res) => {
   return res.json({ message: 'helo world' })
 })
 
-app.listen(3000, () => console.log('✅ Server started at http://localhost:3000 🌏'))
+app.listen(3333, () => console.log('✅ Server started at http://localhost:3333 🌏'))
