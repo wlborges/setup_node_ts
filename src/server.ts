ccd6f66267a67017363b73e813994624b90f1e33
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2023-05-14 22:33:02 -0300

    correção hello world

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 
 const app = express()
 app.get('/', (req, res) => {
-  return res.json({ message: 'helo world' })
+  return res.json({ message: 'Hello world!' })
 })
 
 app.listen(3333, () => console.log('✅ Server started at http://localhost:3333 🌏'))
