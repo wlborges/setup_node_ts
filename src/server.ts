93a6f13ee979d8fb087fda74343488cce0bf1091
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2023-05-14 22:35:12 -0300

    coolify

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 
 const app = express()
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello world!' })
+  return res.json({ message: 'Hello world! Coolify!' })
 })
 
 app.listen(3333, () => console.log('✅ Server started at http://localhost:3333 🌏'))
